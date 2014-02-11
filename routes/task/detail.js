063f6a0982825e55a597f40aca4ff4cd1f5aa819
songsong <xiongsongsong@outlook.com>
2014-02-11 20:09:55 +0800

    增加错误URL时的提示

--- routes/task/detail.js
+++ routes/task/detail.js
@@ -6,8 +6,19 @@ var app = require('app')
 var db = require('db')
 
 app.get(/\/task\/detail\/([a-z0-9]{24})/, function (req, res) {
+
+    //todo:权限验证
+    if (!req.isLogin(req)) {
+        res.redirect('/')
+        return
+    }
+
     var task = new db.Collection(db.Client, 'task')
     task.findOne({_id: db.mongodb.ObjectID(req.params[0]) }, {}, function (err, data) {
-        res.render('task/detail', data)
+        if (data) {
+            res.render('task/detail', data)
+        } else {
+            res.end('404 Not Found')
+        }
     })
 })
