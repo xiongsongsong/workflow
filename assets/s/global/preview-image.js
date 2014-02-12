135129fbbfbccba0d1e04f06fe4d9d1decfdd279
xiongsongsong <xiongsongsong@outlook.com>
2014-02-12 19:24:13 +0800

    修改“下载附件”出错的问题

--- assets/s/global/preview-image.js
+++ assets/s/global/preview-image.js
@@ -17,7 +17,7 @@ define(function (require, exports, module) {
         var $this = $(ev.target)
         if ($this.data('file-id') && /(?:jpg|gif|jpeg|png)$/gi.test($this.data('file-id'))) {
             ev.preventDefault()
-            $preview.html('<img src="' + ev.target.href + '?m=full-size" />')
+            $preview.html('<img src="' + ev.target.href + '" />')
             show()
         }
     })
